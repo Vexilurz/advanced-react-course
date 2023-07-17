ede01ef98b26f9cea06effd7b3f65fc3cdb8f976
Sergei Potekhin <vexilurz@gmail.com>
2023-07-17 17:05:00 +0300

    add TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,19 @@ const webpack = require('webpack');
 module.exports = {
   mode: "development",
   // mode: "production",
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'build'),
