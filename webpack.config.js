bdde638656441ebfa9b5fdf6a424c0251e047c57
Sergei Potekhin <vexilurz@gmail.com>
2023-07-17 16:56:20 +0300

    some webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: "development",
+  // mode: "production",
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'build'),
+    clean: true
   },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),    
+  ]
 };
