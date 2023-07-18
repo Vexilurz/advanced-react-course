113852f91d26b14828284d53f082dc77fd3d6a88
Sergei Potekhin <vexilurz@gmail.com>
2023-07-18 17:26:00 +0300

    css class names readable on dev and only hash on prod

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.scss'
+import {Counter} from "./components/Counter";
+
+const App = () => {
+  return (
+    <div className="app">
+      test
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
