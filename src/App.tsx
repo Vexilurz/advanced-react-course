8063f4dc1bd5f5a824d924637714ce058a9de227
Sergei Potekhin <vexilurz@gmail.com>
2023-07-18 19:16:47 +0300

    connect router and two test pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './index.scss'
-import {Counter} from "./components/Counter";
+import {Link, Route, Routes} from "react-router-dom";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
 
 const App = () => {
   return (
     <div className="app">
-      test
-      <Counter />
+      <Link to={'/'}>Home</Link>
+      <Link to={'/about'}>About</Link>
+      <Routes>
+        <Route path={'/about'} element={<AboutPage />} />
+        <Route path={'/'} element={<MainPage />} />
+      </Routes>
     </div>
   );
 };
