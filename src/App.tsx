26628ca74b7d8e19e324753ce097d5e8d2e0b805
Sergei Potekhin <vexilurz@gmail.com>
2023-07-18 20:15:25 +0300

    toggle light and dark themes (template)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React, { Suspense } from 'react';
+import React, {Suspense, useState} from 'react';
 import {Link, Route, Routes} from "react-router-dom";
 import './styles/index.scss'
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
+export enum Theme {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
+
 const App = () => {
+  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+
+  const toggleTheme = () => {
+    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+  }
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle theme</button>
       <Link to={'/'}>Home</Link>
       <Link to={'/about'}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
