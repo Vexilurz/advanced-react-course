0113d52c180297313e756d8a7e60264a19b8d48e
Sergei Potekhin <vexilurz@gmail.com>
2023-07-18 21:45:39 +0300

    classNames helper function

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import './styles/index.scss'
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 const App = () => {
   const {theme, toggleTheme} = useTheme()
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>Toggle theme</button>
       <Link to={'/'}>Home</Link>
       <Link to={'/about'}>About</Link>
