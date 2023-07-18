4ce27d47102c13cdfdfbf0c5545da80650731fea
Sergei Potekhin <vexilurz@gmail.com>
2023-07-18 19:35:05 +0300

    lazy load pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './index.scss'
 import {Link, Route, Routes} from "react-router-dom";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 const App = () => {
   return (
     <div className="app">
       <Link to={'/'}>Home</Link>
       <Link to={'/about'}>About</Link>
-      <Routes>
-        <Route path={'/about'} element={<AboutPage />} />
-        <Route path={'/'} element={<MainPage />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync />} />
+          <Route path={'/'} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
