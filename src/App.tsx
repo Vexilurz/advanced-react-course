987cfcabe536eb9a94df27875e0423c8de8a46c5
Sergei Potekhin <vexilurz@gmail.com>
2023-07-18 20:46:31 +0300

    Theme: Context, Provider and useTheme hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import React, {Suspense, useState} from 'react';
+import React, {Suspense} from 'react';
 import {Link, Route, Routes} from "react-router-dom";
 import './styles/index.scss'
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-
-export enum Theme {
-  LIGHT = 'light',
-  DARK = 'dark',
-}
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
-  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
-
-  const toggleTheme = () => {
-    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
-  }
+  const {theme, toggleTheme} = useTheme()
 
   return (
     <div className={`app ${theme}`}>
