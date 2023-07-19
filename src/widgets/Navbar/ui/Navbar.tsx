148c38cdf1088bb7ed6118152f5d5124dcdec1d1
Sergei Potekhin <vexilurz@gmail.com>
2023-07-19 19:47:29 +0300

    some styles

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -10,8 +10,10 @@ interface NavbarProps {
 export const Navbar = ({className} : NavbarProps) => {
   return (
     <div className={classNames(cl.navbar, {}, [className])}>
-      <Link to={'/'}>Home</Link>
-      <Link to={'/about'}>About</Link>
+      <div className={cl.links}>
+        <Link to={'/'} className={cl.mainLink}>Home</Link>
+        <Link to={'/about'}>About</Link>
+      </div>
     </div>
   );
 };
