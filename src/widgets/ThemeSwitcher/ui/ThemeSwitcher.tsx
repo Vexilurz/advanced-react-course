ae657a46be15f1da44cf9d35677afd4bd665baf4
Sergei Potekhin <vexilurz@gmail.com>
2023-07-19 21:16:06 +0300

    Button UI Kit

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import {Theme, useTheme} from "app/providers/ThemeProvider";
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+import {Button, ButtonTheme} from "shared/ui/Button/Button";
 
 interface ThemeSwitcherProps {
   className?: string;
@@ -13,7 +14,8 @@ export const ThemeSwitcher = ({className} : ThemeSwitcherProps) => {
   const {theme, toggleTheme} = useTheme()
 
   return (
-    <button
+    <Button
+      theme={ButtonTheme.CLEAR}
       className={classNames(cl.themeSwitcher, {}, [className])}
       onClick={toggleTheme}
     >
@@ -21,6 +23,6 @@ export const ThemeSwitcher = ({className} : ThemeSwitcherProps) => {
         ? <DarkIcon />
         : <LightIcon />
       }
-    </button>
+    </Button>
   );
 };
