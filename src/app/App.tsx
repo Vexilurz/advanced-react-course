b711dc62d7bb3662bacd473f780c2d10417701e5
Sergei Potekhin <vexilurz@gmail.com>
2023-07-20 16:33:58 +0300

    LangSwitcher on sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,22 +5,6 @@ import { useTheme } from './providers/ThemeProvider';
 import { AppRouter } from './providers/router';
 import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar";
-import { useTranslation } from 'react-i18next';
-
-const Component = () => {
-  const { t, i18n } = useTranslation();
-
-  const toggle = () => {
-    i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
-  }
-
-  return (
-    <div>
-      <button onClick={toggle}>{t('translate')}</button>
-      {t('test')}
-    </div>
-  )
-}
 
 const App = () => {
   const {theme} = useTheme()
@@ -29,7 +13,6 @@ const App = () => {
     <div className={classNames('app', {}, [theme])}>
       <Suspense fallback=''>
         <Navbar />
-        <Component/>
         <div className='content-page'>
           <Sidebar/>
           <AppRouter />
