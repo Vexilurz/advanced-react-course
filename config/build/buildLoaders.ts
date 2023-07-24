d9bbc450e2a34fa698f5523246bc6c4b4d09326c
Sergei Potekhin <vexilurz@gmail.com>
2023-07-24 13:54:39 +0300

    babel i18n extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -9,7 +9,16 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     use: {
       loader: "babel-loader",
       options: {
-        presets: ['@babel/preset-env']
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            "i18next-extract",
+            {
+              locales: ['ru', 'en'],
+              keyAsDefaultValue: true
+            }
+          ],
+        ]
       }
     }
   }
