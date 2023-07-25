ba09293ff29f3acb7bfabc5c56ece2e2b78b3ad5
Sergei Potekhin <vexilurz@gmail.com>
2023-07-25 22:21:58 +0300

    refactor spinner to separate shared component

--- src/widgets/PageLoader/ui/PageLoader.tsx
+++ src/widgets/PageLoader/ui/PageLoader.tsx
@@ -1,5 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames'
-import './PageLoader.scss'
+import cl from './PageLoader.module.scss'
+import { Loader } from 'shared/ui/Loader/Loader'
 
 interface PageLoaderProps {
   className?: string
@@ -7,11 +8,8 @@ interface PageLoaderProps {
 
 export const PageLoader = ({ className }: PageLoaderProps) => {
   return (
-    <div className={classNames('page__loader', {}, [className])}>
-      <div className="lds-ripple">
-        <div></div>
-        <div></div>
-      </div>
+    <div className={classNames(cl.pageLoader, {}, [className])}>
+      <Loader />
     </div>
   )
 }
