da6e506f97d28e07c2a0abd89ba4aeffc9bbca58
Sergei Potekhin <vexilurz@gmail.com>
2023-07-26 00:19:51 +0300

    eslint max-len 100

--- .eslintrc.js
+++ .eslintrc.js
@@ -58,7 +58,7 @@ module.exports = {
     'import/no-extraneous-dependencies': 'warn',
     'no-underscore-dangle': 'off',
     'i18next/no-literal-string': ['error', { markupOnly: true }],
-    'max-len': ['error', { ignoreComments: true }]
+    'max-len': ['error', { ignoreComments: true, code: 100 }]
   },
   globals: {
     __IS_DEV__: true
