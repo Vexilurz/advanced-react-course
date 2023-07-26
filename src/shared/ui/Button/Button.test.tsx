d76dcda34260016c5c0fa69af0406eeae89b87a5
Sergei Potekhin <vexilurz@gmail.com>
2023-07-26 19:43:53 +0300

    Button have class test

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,9 +1,15 @@
 import { render, screen } from '@testing-library/react'
-import { Button } from 'shared/ui/Button/Button'
+import { Button, ButtonTheme } from 'shared/ui/Button/Button'
 
 describe('Button', () => {
   test('button is rendered', () => {
     render(<Button>TEST</Button>)
     expect(screen.getByText('TEST')).toBeInTheDocument()
+  })
+
+  test('ButtonTheme.CLEAR', () => {
+    render(<Button theme={ButtonTheme.CLEAR}>TEST</Button>)
+    expect(screen.getByText('TEST')).toHaveClass('clear')
+    // screen.debug()
   })
 })
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -22,6 +22,7 @@ export const Button: FC<ButtonProps> = (props) => {
 
   return (
     <button
+      type="button"
       className={classNames(cl.button, {}, [className, cl[theme]])}
       {...otherProps}
     >
