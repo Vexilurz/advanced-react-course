bbc2b4445c0428390995c5b580e3c5caa3ef3a42
Sergei Potekhin <vexilurz@gmail.com>
2023-07-27 14:42:36 +0300

    Pages stories

---
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator'
+import { Theme } from 'app/providers/ThemeProvider'
+import MainPage from './MainPage'
+
+const meta = {
+  title: 'pages/MainPage',
+  component: MainPage,
+  args: {
+  }
+} satisfies Meta<typeof MainPage>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Light: Story = {
+  args: {
+  }
+}
+
+export const Dark: Story = {
+  args: {
+  },
+  decorators: [
+    ThemeDecorator(Theme.DARK)
+  ]
+}
